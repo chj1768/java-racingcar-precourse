63b874102d2d4efa9ebb1a3f58a123f7a76bbf1a
chj1768 <chj1768@naver.com>
2022-04-25 00:17:12 +0900

    fix: Locations 가 Integer 타입 대신 Position 타입을 사용하도록 변경

--- src/main/java/racingcar/Locations.java
+++ src/main/java/racingcar/Locations.java
@@ -1,7 +1,51 @@
 package racingcar;
 
+import java.util.Collections;
 import java.util.LinkedHashMap;
 
-public class Locations extends LinkedHashMap<Car, Integer> {
+public class Locations extends LinkedHashMap<Car, Position> {
+	public Locations(Cars cars) {
+		for (Car car : cars) {
+			this.put(car, new Position());
+		}
+	}
 
+	public Position getPosition(Car car) {
+		Position position = this.get(car);
+		if (position == null)
+			throw new IllegalArgumentException("컨트롤타워가 관찰중인 자동차가 아닙니다.");
+
+		return position;
+	}
+
+	public void update(Car car) {
+		Position position = getPosition(car);
+		if (!car.isStopped())
+			position.right();
+	}
+
+	public Cars getAll() {
+		Cars cars = new Cars();
+		cars.addAll(this.keySet());
+		return cars;
+	}
+
+	public Cars winners() {
+		Position winner = getWinner();
+		Cars cars = getAll();
+		Cars winners = new Cars();
+		for (Car car : cars) {
+			checkAndSetWinner(winner, car, winners);
+		}
+		return cars;
+	}
+
+	private Position getWinner() {
+		return Collections.max(this.values());
+	}
+
+	private void checkAndSetWinner(Position winner, Car car, Cars winners) {
+		if (winner.compareTo(this.get(car)) == 0)
+			winners.add(car);
+	}
 }
